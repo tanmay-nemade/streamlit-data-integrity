4c10a6cbdf328fc46c7a70ebb3fc5d7657227659
Tanmay Nemade <tanmay.nemade@ad.infosys.com>
2023-03-28 17:48:36 +0530

    edit

--- main.py
+++ main.py
@@ -1,2 +1,43 @@
-'''This is a Test Streamlit App'''
-123456
+import os
+import configparser
+from snowflake.snowpark import Session
+from snowflake.snowpark.functions import *
+import pandas as pd
+import streamlit as st
+
+st.set_page_config(
+    layout="wide",
+    page_title="Streamlit Data Integrity"
+)
+
+st.title('Streamlit Data Integrity App')
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+sections = config.sections()
+accounts = []
+for section in sections:
+    accounts.append(section)
+
+def sfAccount_selector(account):
+    #setup config.ini read rules
+    sfAccount = config[account]['sfAccount']
+    sfUser = config[account]['sfUser']
+    sfPass = config[account]['sfPass']
+    sfRole = config[account]['sfRole']
+    #sfDB = config[account]['sfDB']
+    #sfSchema = config[account]['sfSchema']
+    sfWarehouse = config[account]['sfWarehouse']
+
+    #dictionary with names and values of connection parameters
+    conn = {"account": sfAccount,
+            "user": sfUser,
+            "password": sfPass,
+            "role": sfRole,
+            "warehouse": sfWarehouse}
+            #"database": sfDB,
+            #"schema": sfSchema}
+
+    #Create a session using the connection parameters
+    session = Session.builder.configs(conn).create()
+    return session
